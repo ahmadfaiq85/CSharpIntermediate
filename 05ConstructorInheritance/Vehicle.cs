95efd3b691b7d7e61d4805e4246105a7a5c617ea
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2017-11-07 16:28:10 -0500

    Constructor and Inheritance

---
+++ 05ConstructorInheritance/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorInheritance
+{
+    public class Vehicle
+    {
+        public string _registrationNumber { get; set; }
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+
+            Console.WriteLine($"Vehicle is being initialized. {registrationNumber}");
+        }
+    }
+}
