1630602237a6d9a4b4ddfb3a80de476161c2c060
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2017-11-07 15:01:40 -0500

    Move Post to a separate file.

--- 02StackOverFlowPost/Program.cs
+++ 02StackOverFlowPost/Program.cs
@@ -6,47 +6,7 @@ using System.Threading.Tasks;
 
 namespace StackOverFlowPost
 {
-    public class Post
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public DateTime CreatedDateTime { get; set; }
-
-        private int _vote { get; set; }
-
-        public Post()
-        {
-            CreatedDateTime = DateTime.Now;
-        }
-        public Post(string title) : this()
-        {
-            Title = title;
-        }
-
-        public Post(string title, string description)
-            : this(title)
-        {
-            Description = description;
-        }
-
-        public void VoteUp()
-        {
-            _vote++;
-        }
-
-        public void VoteDown()
-        {
-            _vote--;
-        }
-        public int Vote
-        {
-            get
-            {
-                return _vote;
-            }
-        }
-
-    }
+    
     class Program
     {
         static void Main(string[] args)
