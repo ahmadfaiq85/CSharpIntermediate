f9f0ac7af3225ada24bcb77ef7803d27764162f1
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2017-11-07 14:39:45 -0500

    Move StopWatch to a separate file

--- 01StopWatch/Program.cs
+++ 01StopWatch/Program.cs
@@ -7,40 +7,6 @@ using System.Threading.Tasks;
 
 namespace StopWatch
 {
-    public class StopWatch
-    {
-        private bool _running { get; set; }
-        private DateTime _startTime { get; set; }
-        private DateTime _endTime { get; set; }
-
-        public void Start()
-        {
-            if (_running)
-                throw new InvalidOperationException("The Stop Watch is already running.");
-
-            Console.WriteLine("Started.");
-            _startTime = DateTime.Now;
-            _running = true;
-        }
-
-        public void Stop()
-        {
-            Console.WriteLine("Stopped.");
-            _endTime = DateTime.Now;
-            _running = false;
-        }
-
-        public TimeSpan Duration
-        {
-            get
-            {
-                if (_startTime == null || _endTime == null)
-                    throw new InvalidOperationException("Stop Watch must first run and stop to get duration.");
-
-                return _endTime - _startTime; 
-            }
-        }
-    }
     class Program
     {
         static void Main(string[] args)
