537521ff57ea95ffa9ded7ddafdfc248c1c0d61e
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2017-11-07 15:20:01 -0500

    Inheritance

---
+++ 03Inheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Text text = new Text();
+
+            text.FontName = "Verdana";
+            text.FontSize = 12;
+            text.AddHyperlink("www.google.com");
+            text.Copy();
+        }
+    }
+}
---
+++ 03Inheritance/Text.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Text : PresentationObject
+    {
+        public string FontName { get; set; }
+        public int FontSize { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine($"Hyperlink added to {url}");
+        }
+
+    }
+}
