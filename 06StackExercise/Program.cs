7b1612e30d72b969b47161544d4286be51e786d5
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2017-11-09 16:13:32 -0500

    Completed the stack excercise

---
+++ 06StackExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            stack.Push(1);
+            stack.Push("Faiq");
+            stack.Push(true);
+            //stack.Push(null);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ 06StackExercise/Stack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExercise
+{
+    public class Stack
+    {
+        List<object> _stack = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Object cannot be null");
+            _stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_stack.Count == 0)
+                throw new InvalidOperationException("No item in stack to Pop");
+
+            object obj = _stack[_stack.Count - 1];
+            _stack.Remove(obj);
+            return obj;
+        }
+    }
+
+}
