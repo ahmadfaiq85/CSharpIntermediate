e49da37045364b76ed8a04a391c61f1b5c1bdf7b
Ahmad Faiq Ateel <ahmadfaiq1@gmail.com>
2017-11-12 13:56:09 -0500

    Method Overriding

---
+++ 07MethodOverriding/Canvas.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MethodOverriding
+{
+    internal class Canvas
+    {
+        public void DrawShapes(List<Shape> shapes)
+        {
+            foreach(Shape shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+}
---
+++ 07MethodOverriding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method overriding the changing the implementation of an inherited method
+            //If we declare a method in the base class, we can override it in the derived class
+            //This technic allows us to achieve polymorphism that is object is a shape but have different
+            //draw implementation
+
+            List<Shape> shapes = new List<Shape>
+            {
+                new Circle { Width = 10, Height = 20 },
+                new Rectangle { Width = 20, Height = 30 },
+                new Triangle {Width = 50, Height = 50 }
+            };
+
+            var canvas = new Canvas();
+            canvas.DrawShapes(shapes);
+        }
+    }
+}
